3f696a519ce100ed3e01640707bdeede70a2efd2
ngnhanbao <baonhanltv3669@gmail.com>
2026-04-21 12:47:35 +0700

    Route Login

--- CRUD-NhomI/routes/web.php
+++ CRUD-NhomI/routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+// Route để hiện trang đăng nhập
+Route::get('/login', function () {
+    return view('login');
+});
+
+// Route để xử lý khi nhấn nút Đăng nhập (POST)
+Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
 Route::get('/register', function () {
     return view('register');
 });
